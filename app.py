450d2a97c4849ecc007be8a56f1f17e3f75e2167
GhostByteX-Ed <141905460+GhostByteX-Ed@users.noreply.github.com>
2023-09-12 14:32:11 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask,render_template
+import pandas as pd
+from pandas_profiling import ProfileReport
+
+app = Flask('__name__')
+@app.route('/')
+def home():
+   
+    data = pd.read_csv("IRIS.csv")
+    profile = ProfileReport(data)
+    print(profile)
+    profile.to_file("Profiling_Report_Results.html")
+    return render_template("Profiling_Report_Results.html")
+
+if(__name__=='__main__'):
+    app.run(debug=True,host="0.0.0.0",port=5000)
+
