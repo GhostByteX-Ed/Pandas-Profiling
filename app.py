51743cac6dd4c0d15026afb20ec4485946de83bf
GhostByteX-Ed <141905460+GhostByteX-Ed@users.noreply.github.com>
2023-09-12 14:54:14 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask,render_template
 import pandas as pd
 from pandas_profiling import ProfileReport
 
-app = Flask('__name__')
+app = Flask('__name__',template_folder='template')
 @app.route('/')
 def home():
    
