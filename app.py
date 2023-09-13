220e793e7f1ffcb64164c1de5071d606191fb890
GhostByteX-Ed <141905460+GhostByteX-Ed@users.noreply.github.com>
2023-09-13 09:58:34 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template
 import pandas as pd
-from pandas_profiling import ProfileReport
+from ydata_profiling import ProfileReport
 
 app = Flask('__name__',template_folder='template')
 @app.route('/')
@@ -8,10 +8,8 @@ def home():
    
     data = pd.read_csv("IRIS.csv")
     profile = ProfileReport(data)
-    print(profile)
     profile.to_file("Profiling_Report_Results.html")
     return render_template("Profiling_Report_Results.html")
 
 if(__name__=='__main__'):
     app.run(debug=True,host="0.0.0.0",port=5000)
-
