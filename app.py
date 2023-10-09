b99122f1a695a7d5f02b0bcdd25f34e32b0a3549
GhostByteX-Ed <141905460+GhostByteX-Ed@users.noreply.github.com>
2023-10-09 08:03:28 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template
 import pandas as pd
 from ydata_profiling import ProfileReport
+import logging,sys
 
 app = Flask('__name__',template_folder='template')
 @app.route('/')
@@ -12,4 +13,8 @@ def home():
     return render_template("Profiling_Report_Results.html")
 
 if(__name__=='__main__'):
+    log = logging.getLogger('werkzeug')
+    log.disabled = True
+    cli = sys.modules['flask.cli']
+    cli.show_server_banner = lambda *x: None
     app.run(debug=True,host="0.0.0.0",port=5000)
